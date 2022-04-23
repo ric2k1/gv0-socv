dab116b12fd7e319c6b61cf9c713ba587f8c6ece
pseudo-aloha <b07901075@ntu.edu.tw>
2022-04-23 18:25:53 +0800

    new test_gen.py

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -28,12 +28,18 @@ with open("gen_pattern.vcd", "w") as f:
                     # print(" n" + str(n_count) + "\n")
                     n_count += 1
                 else:
+                    # print(len(text[:text.index("n") - 1]))
+                    text = text[:text.index("n") - 1]
                     for t in text:
-                        if t == "x":
+                        if t == "x" or t == "0" or t == "1":
                             count += 1
                     f.write("0" * count)
                     f.write(" n" + str(n_count) + "\n")
                     n_count += 1
             if text == "" or text == "#0\n":
                 time0_start = True
+    for i in range(1, 3000):
+        f.write("#" + str(100 * i) + "\n")
+        f.write("b" + str(i % 2) + " n6\n")
+        f.write("b1 n25\n")
     
