bea9da933e77bd6b6c2bada7642b1c9c723a9f1e
HHHUUUGGGOOO <hugo551565@gmail.com>
2022-07-13 11:48:42 +0800

    v3 install

--- install_sh/v3_install.sh
+++ install_sh/v3_install.sh
@@ -7,5 +7,6 @@ git clone https://github.com/chengyinwu/V3
 rm -rf V3/engine
 cp -rf v3_needed/SoCV_v3_engine V3/engine
 cp v3_needed/Makefile.in.diff V3/src
-cd V3/engine/; ./boolector.script; ./minisat.script; ./quteRTL.script; cd ../
+cd V3/engine/; chmod +x boolector.script; chmod +x minisat.script; chmod +x quteRTL.script
+./boolector.script; ./minisat.script; ./quteRTL.script; cd ../
 cd src; patch Makefile.in < Makefile.in.diff; cd ..; make;
