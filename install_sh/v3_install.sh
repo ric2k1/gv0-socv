74ff9b25e0d00b9adcdee99cc4ad38546c7eb24e
HHHUUUGGGOOO <45988775+HHHUUUGGGOOO@users.noreply.github.com>
2022-07-13 23:50:52 +0800

    Update v3_install.sh

--- install_sh/v3_install.sh
+++ install_sh/v3_install.sh
@@ -1,12 +1,7 @@
-# sudo apt-get install git
-# sudo apt-get install wget
-# sudo apt-get install libreadline-dev
-# sudo apt-get install graphviz
+git clone https://github.com/HHHUUUGGGOOO/V3-fixed.git
+cd V3-fixed/
+make
+cd ../
 
-git clone https://github.com/chengyinwu/V3
-rm -rf V3/engine
-cp -rf v3_needed/SoCV_v3_engine V3/engine
-cp v3_needed/Makefile.in.diff V3/src
-cd V3/engine/; chmod +x boolector.script; chmod +x minisat.script; chmod +x quteRTL.script
-./boolector.script; ./minisat.script; ./quteRTL.script; cd ../
-cd src; patch Makefile.in < Makefile.in.diff; cd ..; make;
+# clear
+clear
