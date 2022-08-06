22460c56ad3af37f61eee851d2d41d752f446f61
hchchiu <robert890619@gmail.com>
2022-08-07 00:38:22 +0800

    update install.sh

--- GV_INSTALL_NEW.sh
+++ GV_INSTALL_NEW.sh
@@ -1,6 +1,6 @@
 cd GV/engine/abc; mkdir gv_src;sudo make -j12;sudo make libabc.a; mv libabc.a gv_src;
 cd ../yosys; 
-make config-gcc;sudo make -j12; sudo make install;
+make config-clang;sudo make -j12; sudo make install;
 ar rc gv_src/libyosys.a backends/*/*.o frontends/*/*.o kernel/*.o libs/*/*.o passes/*/*.o techlibs/*/*.o;
 cd ../..;
 sudod make clean;sudo make;./gv
