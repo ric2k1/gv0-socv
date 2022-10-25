2d094e9698c3f3b9926db9d42998d71b6749cf27
R10943169 <r10943169@ntu.edu.tw>
2022-10-25 23:41:46 +0800

    Extract RTL info

--- src/sim/gvSimCmd.cpp
+++ src/sim/gvSimCmd.cpp
@@ -12,7 +12,8 @@
 bool GVinitSimCmd() {
     return (
          gvCmdMgr->regCmd("RAndom Sim",   2, 1, new GVRandomSimCmd   ) &&
-         gvCmdMgr->regCmd("SEt SAfe"  ,   2, 2, new GVRandomSetSafe  ) 
+         gvCmdMgr->regCmd("SEt SAfe"  ,   2, 2, new GVRandomSetSafe  ) &&
+         gvCmdMgr->regCmd("CRP G",        3, 1, new GVCRPG)
     );
 }
 
@@ -148,4 +149,102 @@ GVRandomSetSafe::help() const {
     gvMsg(GV_MSG_IFO) << setw(20) << left << "SEt SAfe: " << "Set safe rpoperty for random sim." << endl;
 }
 
+//----------------------------------------------------------------------
+// CRPG
+//----------------------------------------------------------------------
+
+// simplest mkdir
+void
+GVmkdir(const string& folder_path){
+    string remove_cmd = "rm -fr " + folder_path;
+    string build_cmd = "mkdir -p " + folder_path;
+    system(remove_cmd.c_str());
+    system(build_cmd.c_str());
+}
+
+// CRPG step 1. Initialize
+void
+CRPG_init(string build_path, string work_path){
+    gvMsg(GV_MSG_IFO) << "[INFO] Initializing build file" << endl;
+    GVmkdir(build_path);
+    GVmkdir(work_path);
+}
+
+// CRPG step 2. Extract RTL info
+void
+CRPG_getDesignInfo(string top_module, string build_path){
+    gvMsg(GV_MSG_IFO) << "[INFO] Extracting information from design" << endl;
+    run_pass("prep -top " + top_module);
+    run_pass("proc_clean");
+    run_pass("proc_init");
+    run_pass("proc_arst");
+    run_pass("proc_mux");
+    run_pass("proc_dff");
+    run_pass("proc_clean");
+    run_pass("fsm_detect");
+    run_pass("fsm_extract");
+    run_pass("fsm_info");
+    run_pass("proc");
+    run_pass("write_json " + build_path + "/design_under_test.json");
+}
+
+// CRPG step 3. DUT preprocessing
+void
+CRPG_dutPreprocessing(string build_path){
+    gvMsg(GV_MSG_IFO) << "[INFO] DUT preprocessing..." << endl;
+    ofstream ofs_assertion_hpp;
+    string dpi_template = \
+    "// DPI Header\
+    import \"DPI-C\" function void set_assert_flag (input int index);\
+    import \"DPI-C\" function void set_constraint_flag (input int index);\
+    import \"DPI-C\" function void set_stable_flag (input int index);\
+    ";
+    vector<RTLIL::Module*> modules_vec = yosys_design->modules().to_vector();
+    for(int i= 0; i< modules_vec.size(); i++){
+        cout << modules_vec[i]->name.str() << endl;
+    }
+}
+
+GVCmdExecStatus
+GVCRPG ::exec(const string& option) {
+    gvMsg(GV_MSG_IFO) << "[INFO] I am GVCRPG " << endl;
+    
+    string top_module = yosys_design->top_module()->name.str().substr(1,strlen(yosys_design->top_module()->name.c_str()) - 1);
+    
+    string curr_path = getcwd(NULL, 0);
+    string build_path = curr_path + "/build";
+    string work_path = curr_path + "/work";
+    
+    // Step 1. Initialize
+    CRPG_init(build_path, work_path);
+    // Step 2. Extract RTL info
+    CRPG_getDesignInfo(top_module, build_path);
+    // Step 3. DUT preprocessing
+    CRPG_dutPreprocessing(build_path);
+    
+    /*
+    // TODO
+    // 3. Use verilator to generate C++ simulator code
+    gvMsg(GV_MSG_IFO) << "[INFO] Generating C++ simulator code" << endl;
+    string VFLAGS;
+    VFLAGS = "--cc --public --trace --assert -O3 ";
+    VFLAGS += "-CFLAGS -o3 ";
+    VFLAGS += "-CFLAGS -I/home/clare/gv0/src/sim/include ";
+    VFLAGS += "-y build ";
+    VFLAGS += "--Mdir build/obj_dir ";
+    VFLAGS += "--exe " + work_path + "/main.cpp";
+    script_cmd = "verilator " + VFLAGS + " build/design_under_test.v";
+    //system(script_cmd.c_str());
+    return GV_CMD_EXEC_DONE;*/
+}
+
+void
+GVCRPG ::usage(const bool& verbose) const {
+    gvMsg(GV_MSG_IFO) << "Usage: CRPG" << endl;
+}
+
+void
+GVCRPG ::help() const {
+    gvMsg(GV_MSG_IFO) << setw(20) << left << "CRPG: " << "Constrained Random Pattern Generation" << endl;
+}
 #endif
