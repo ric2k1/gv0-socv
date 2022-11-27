56fcfcec45bfadf7d41306a99d97330bf8b7ffe6
hchchiu <robert890619@gmail.com>
2022-11-27 23:29:02 +0800

    Modify install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,30 +7,33 @@ cd engine/;
 ./abc.script;
 # install "yosys"
 ./yosys.script;
+# install "boolector"
+./boolector.script
+
 
 # back to gv0/
 cd ../;
 
 #ln -s  ./engine/boolector/build/lib ./src/eng/boolector
 #clone boolector
-cd engine/; git clone https://github.com/Boolector/boolector.git;
-cd boolector ;
-#build dependend tool
-./contrib/setup-lingeling.sh ;
-./contrib/setup-btor2tools.sh ;
-#build boolector
-./configure.sh --no-minisat ; cd build; make;
-#copy lib&header to src/eng
-cd ../../../
-#
-cp ./engine/boolector/build/lib/libboolector.a  ./src/eng/boolector/
-cp ./engine/boolector/src/boolector.h  ./src/eng/boolector/
-cp ./engine/boolector/src/btortypes.h  ./src/eng/boolector/
+# cd engine/; git clone https://github.com/Boolector/boolector.git;
+# cd boolector ;
+# #build dependend tool
+# ./contrib/setup-lingeling.sh ;
+# ./contrib/setup-btor2tools.sh ;
+# #build boolector
+# ./configure.sh --no-minisat ; cd build; make;
+# #copy lib&header to src/eng
+# cd ../../../
+# #
+# cp ./engine/boolector/build/lib/libboolector.a  ./src/eng/boolector/
+# cp ./engine/boolector/src/boolector.h  ./src/eng/boolector/
+# cp ./engine/boolector/src/btortypes.h  ./src/eng/boolector/
 
-mkdir ./src/eng/btor2parser
-mkdir ./src/eng/lgl
-cp ./engine/boolector/deps/install/lib/libbtor2parser.a  ./src/eng/btor2parser/
-cp ./engine/boolector/deps/install/lib/liblgl.a  ./src/eng/lgl
+# mkdir ./src/eng/btor2parser
+# mkdir ./src/eng/lgl
+# cp ./engine/boolector/deps/install/lib/libbtor2parser.a  ./src/eng/btor2parser/
+# cp ./engine/boolector/deps/install/lib/liblgl.a  ./src/eng/lgl
 
 # make GV and get started
 make clean; make; make; clear; ./gv;
