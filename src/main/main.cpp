ce7da4acb63ec22e290940adf5a90b3212e2e8de
hchchiu <robert890619@gmail.com>
2022-11-28 19:54:58 +0800

    Format the main.cpp

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -9,14 +9,14 @@
 #ifndef GV_MAIN_CC
 #define GV_MAIN_CC
 
-#include "util.h"
 #include "gvCmdMgr.h"
 #include "gvMsg.h"
 #include "gvUsage.h"
 #include "kernel/yosys.h"
+#include "util.h"
 using namespace std;
 
-string GVMsg::_allName = "";
+string   GVMsg::_allName = "";
 ofstream GVMsg::_allout;
 GVMsgMgr gvMsg;
 
@@ -34,56 +34,48 @@ extern bool GVinitVrfCmd();
 extern bool GVinitAbcCmd();
 extern bool GVinitModCmd();
 
-
-
 static void
-usage()
-{
-   cout << "Usage: cirTest [ -File < doFile > ]" << endl;
+usage() {
+    cout << "Usage: cirTest [ -File < doFile > ]" << endl;
 }
 
 static void
-myexit()
-{
-   usage();
-   exit(-1);
+myexit() {
+    usage();
+    exit(-1);
 }
 
 int
-main(int argc, char** argv)
-{
-   myUsage.reset();
+main(int argc, char** argv) {
+    myUsage.reset();
 
-   ifstream dof;
+    ifstream dof;
 
-   if (argc == 3) {  // -file <doFile>
-      if (myStrNCmp("-File", argv[1], 2) == 0) {
-         if (!gvCmdMgr->openDofile(argv[2])) {
-            cout << "Error: cannot open file \"" << argv[2] << "\"!!\n";
+    if (argc == 3) { // -file <doFile>
+        if (myStrNCmp("-File", argv[1], 2) == 0) {
+            if (!gvCmdMgr->openDofile(argv[2])) {
+                cout << "Error: cannot open file \"" << argv[2] << "\"!!\n";
+                myexit();
+            }
+        } else {
+            cout << "Error: unknown argument \"" << argv[1] << "\"!!\n";
             myexit();
-         }
-      }
-      else {
-         cout << "Error: unknown argument \"" << argv[1] << "\"!!\n";
-         myexit();
-      }
-   }
-   else if (argc != 1) {
-      cout << "Error: illegal number of argument (" << argc << ")!!\n";
-      myexit();
-   }
-   Yosys::yosys_setup(); // initial yosys command
-   Yosys::log_streams.push_back(&std::cout); // log yosys message
-   if (!(GVinitCommonCmd() && GVinitNtkCmd() && GVinitSimCmd() && GVinitVrfCmd() && GVinitAbcCmd() && GVinitModCmd()))
-      return 1;
+        }
+    } else if (argc != 1) {
+        cout << "Error: illegal number of argument (" << argc << ")!!\n";
+        myexit();
+    }
+    Yosys::yosys_setup();                     // initial yosys command
+    Yosys::log_streams.push_back(&std::cout); // log yosys message
+    if (!(GVinitCommonCmd() && GVinitNtkCmd() && GVinitSimCmd() && GVinitVrfCmd() && GVinitAbcCmd() && GVinitModCmd())) return 1;
 
-   GVCmdExecStatus status = GV_CMD_EXEC_DONE;
-   while (status != GV_CMD_EXEC_QUIT) {
-      gvCmdMgr->setPrompt();
-      status = gvCmdMgr->execOneCmd();
-      cout << endl;
-   }
-   return 0;
+    GVCmdExecStatus status = GV_CMD_EXEC_DONE;
+    while (status != GV_CMD_EXEC_QUIT) {
+        gvCmdMgr->setPrompt();
+        status = gvCmdMgr->execOneCmd();
+        cout << endl;
+    }
+    return 0;
 }
 
 #endif
