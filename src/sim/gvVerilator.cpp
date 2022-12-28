1066f65d316bbe9ed3c67e21fb1a29b5c9850232
R10943169 <r10943169@ntu.edu.tw>
2022-12-29 02:00:38 +0800

    clang format

--- src/sim/gvVerilator.cpp
+++ src/sim/gvVerilator.cpp
@@ -1,29 +1,28 @@
-#include <string>
-#include <vector>
 #include <iostream>
 #include <regex>
+#include <string>
+#include <vector>
 
-#include "gvSimCmd.h"
 #include "gvMsg.h"
 #include "gvNtk.h"
-#include "json.hpp"
-#include <sys/types.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
+#include "gvSimCmd.h"
 #include "gvVerilator.hpp"
-
+#include "json.hpp"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
 using json = nlohmann::json;
 
 void
-VerilatorAPI::writeData(string data){
+VerilatorAPI::writeData(string data) {
     //// IPC - Shared Memory
-    VerilatorAPI::shm= (char*) shmat(VerilatorAPI::get_shmid(), NULL, 0);
-    if(VerilatorAPI::shm == (char*) -1) {
+    VerilatorAPI::shm = (char*)shmat(VerilatorAPI::get_shmid(), NULL, 0);
+    if (VerilatorAPI::shm == (char*)-1) {
         cout << strerror(errno) << endl;
         perror("Error in Shared Memory attachment");
         exit(1);
@@ -36,7 +35,7 @@ VerilatorAPI::writeData(string data){
 }
 
 void
-VerilatorAPI::update(){
+VerilatorAPI::update() {
     system("obj_dir/Vtestf --update");
 }
 
@@ -51,10 +50,10 @@ VerilatorAPI::printState() {
 }
 
 string
-VerilatorAPI::readData(){
+VerilatorAPI::readData() {
     //// IPC - Shared Memory
-    VerilatorAPI::shm= (char*) shmat(VerilatorAPI::get_shmid(), NULL, 0);
-    if(VerilatorAPI::shm == (char*) -1) {
+    VerilatorAPI::shm = (char*)shmat(VerilatorAPI::get_shmid(), NULL, 0);
+    if (VerilatorAPI::shm == (char*)-1) {
         cout << strerror(errno) << endl;
         perror("Error in Shared Memory attachment");
         exit(1);
@@ -63,51 +62,46 @@ VerilatorAPI::readData(){
     string pattern_str(VerilatorAPI::shm);
     string token;
     size_t pos = pattern_str.find(" ");
-    token = pattern_str.substr(0, pos);
+    token      = pattern_str.substr(0, pos);
     cout << "[INFO] GV - Shared memory (id=" << VerilatorAPI::get_shmid() << ") => Read data (size=" << token.size() << ")" << endl;
     cout << token << endl;
     return token;
 }
 
 void
-VerilatorAPI::getSequence(){
+VerilatorAPI::getSequence() {
     system("obj_dir/Vtestf --sequence");
 }
 
-void 
-VerilatorAPI::reset(){
+void
+VerilatorAPI::reset() {
     system("obj_dir/Vtestf --reset");
 }
 
 // Extracting all key from map
 vector<string>
-getAllKey(json myJson){
+getAllKey(json myJson) {
     vector<string> keys;
-    for (auto& item : myJson.items()){
+    for (auto& item : myJson.items()) {
         keys.push_back(item.key());
     }
-    //for (map<string, string>::iterator it = myMap.begin(); it != myMap.end(); it++){
-    //    keys.push_back(it->first);
-    //}
+    // for (map<string, string>::iterator it = myMap.begin(); it != myMap.end(); it++){
+    //     keys.push_back(it->first);
+    // }
     return keys;
 }
 
 string
-signalWidth2Type(string width){
+signalWidth2Type(string width) {
     int width_int = stoi(width);
-    if (width_int <= 8)
-        return "C";
-    else if (width_int <= 16)
-        return "S";
-    else if (width_int <= 32)
-        return "I";
-    else if (width_int <= 64)
-        return "Q";
-    else
-        return "W";
+    if (width_int <= 8) return "C";
+    else if (width_int <= 16) return "S";
+    else if (width_int <= 32) return "I";
+    else if (width_int <= 64) return "Q";
+    else return "W";
 }
 
-void 
+void
 VerilatorAPI::verilogModeling(string testbed) {
     gvMsg(GV_MSG_IFO) << "[INFO] Generating Verilator simulator code" << endl;
 
@@ -115,23 +109,23 @@ VerilatorAPI::verilogModeling(string testbed) {
     VFLAGS = "--cc -sv -Wno-lint --build --public --trace --assert -O3 ";
     VFLAGS += "--exe " + testbed;
     VFLAGS += " -CFLAGS -o3 ";
-    //VFLAGS += "-CFLAGS -I/home/clare/gv0/src/sim/include ";
-    //VFLAGS += "-y build ";
-    //VFLAGS += "--Mdir build/obj_dir ";
-    //VFLAGS += "--main ";
-    //VFLAGS += "--top-module " + top_module;
-    //system_cmd = "verilator " + VFLAGS + " build/design_under_test.v";
+    // VFLAGS += "-CFLAGS -I/home/clare/gv0/src/sim/include ";
+    // VFLAGS += "-y build ";
+    // VFLAGS += "--Mdir build/obj_dir ";
+    // VFLAGS += "--main ";
+    // VFLAGS += "--top-module " + top_module;
+    // system_cmd = "verilator " + VFLAGS + " build/design_under_test.v";
     system_cmd = "verilator " + VFLAGS + gvModMgr->getInputFileName();
     gvMsg(GV_MSG_DBG) << "[DBG] Verilator command -> " << system_cmd << endl;
-    system(system_cmd.c_str());  
+    system(system_cmd.c_str());
 }
 
 // Extracting RTL info by yosys
 string
-VerilatorAPI::getDesignInfo(){
+VerilatorAPI::getDesignInfo() {
     string output_path = "obj_dir/design_under_test.json";
     gvMsg(GV_MSG_IFO) << "[INFO] Extracting information from design => " << output_path << endl;
-    run_pass("prep"); 
+    run_pass("prep");
     run_pass("proc_clean");
     run_pass("proc_init");
     run_pass("proc_arst");
@@ -147,70 +141,67 @@ VerilatorAPI::getDesignInfo(){
 }
 
 void
-VerilatorAPI::genInterfaceFile(){
+VerilatorAPI::genInterfaceFile() {
     gvMsg(GV_MSG_IFO) << "[INFO] Generating interface.hpp ..." << endl;
     ifstream input_file("obj_dir/design_under_test.json");
     ofstream output_file("obj_dir/interface.hpp");
-    if (!input_file.good())
-        gvMsg(GV_MSG_ERR) << "[ERR] design_under_test.json doesn't exist" << endl;
-    else{
-        json rtl_info;
-        vector<string> input_vec, output_vec, net_vec, key_vec;
+    if (!input_file.good()) gvMsg(GV_MSG_ERR) << "[ERR] design_under_test.json doesn't exist" << endl;
+    else {
+        json                rtl_info;
+        vector<string>      input_vec, output_vec, net_vec, key_vec;
         map<string, string> name2bit_map, module2cell;
-        string port_type, port_name, port_bits, module_name, net_name, net_bits, cell_name, cell_type;
-        regex dot("\\.");
+        string              port_type, port_name, port_bits, module_name, net_name, net_bits, cell_name, cell_type;
+        regex               dot("\\.");
         rtl_info << input_file;
         key_vec = getAllKey(rtl_info["modules"]);
-        
+
         // get PI/PO
-        for (auto& port : rtl_info["modules"]["test"]["ports"].items()){
+        for (auto& port : rtl_info["modules"]["test"]["ports"].items()) {
             port_name = port.key();
             port_type = port.value()["direction"];
             port_bits = to_string(rtl_info["modules"]["zipsystem"]["ports"][port_name]["bits"].size());
-            if (port_type == "input"){
+            if (port_type == "input") {
                 input_vec.push_back(port_name);
                 name2bit_map[port_name] = port_bits;
                 // cout << port_name << endl;
-                //type_info &ai = typeid(rtl_info["modules"]["zipsystem"]["ports"][key]["bits"]);
-                //cout << rtl_info["modules"]["zipsystem"]["ports"][key]["bits"].size() << endl;
-            }
-            else if (port_type == "output"){
+                // type_info &ai = typeid(rtl_info["modules"]["zipsystem"]["ports"][key]["bits"]);
+                // cout << rtl_info["modules"]["zipsystem"]["ports"][key]["bits"].size() << endl;
+            } else if (port_type == "output") {
                 output_vec.push_back(port_name);
                 name2bit_map[port_name] = port_bits;
             }
-                //cout << "Port name: " << port_name << endl;
-                //cout << "Port type: " << port_type << endl;
-                //cout << "Port bits: " << port_bits << endl;    
+            // cout << "Port name: " << port_name << endl;
+            // cout << "Port type: " << port_type << endl;
+            // cout << "Port bits: " << port_bits << endl;
         }
-        
+
         // get net's info
-        for (auto& module : rtl_info["modules"].items()){
-            module_name = module.key(); 
-            for (auto& net : rtl_info["modules"][module_name]["netnames"].items()){
+        for (auto& module : rtl_info["modules"].items()) {
+            module_name = module.key();
+            for (auto& net : rtl_info["modules"][module_name]["netnames"].items()) {
                 net_name = net.key();
                 net_name = regex_replace(net_name, dot, "__DOT__");
                 net_bits = to_string(rtl_info["modules"][module_name]["netnames"][net_name]["bits"].size());
                 net_vec.push_back(net_name);
                 name2bit_map[net_name] = net_bits;
-                //cout << "Module name: " << module_name << endl;
-                //cout << "Net name: " << net_name << endl;
-                //cout << "Net bits: " << net_bits << endl;
-                //for (const auto& val : net.value().items())
-                //    std::cout << "  " << val.key() << ": " << val.value() << "\n";
+                // cout << "Module name: " << module_name << endl;
+                // cout << "Net name: " << net_name << endl;
+                // cout << "Net bits: " << net_bits << endl;
+                // for (const auto& val : net.value().items())
+                //     std::cout << "  " << val.key() << ": " << val.value() << "\n";
             }
-            for (auto& cell : rtl_info["modules"][module_name]["cells"].items()){
+            for (auto& cell : rtl_info["modules"][module_name]["cells"].items()) {
                 cell_name = cell.key();
                 cell_type = rtl_info["modules"][module_name]["cells"][cell_name]["type"];
-                if (find(key_vec.begin(), key_vec.end(), cell_type) != key_vec.end()){
+                if (find(key_vec.begin(), key_vec.end(), cell_type) != key_vec.end()) {
                     module2cell[cell_type] = cell_name;
-                    //cout << "test" << cell_type << "   " << cell_name << endl;
+                    // cout << "test" << cell_type << "   " << cell_name << endl;
                 }
-                //cout << "Cell name: " << cell_name << endl;
-                //cout << "Cell type: " << cell_type << endl;
+                // cout << "Cell name: " << cell_name << endl;
+                // cout << "Cell type: " << cell_type << endl;
             }
-                    
         }
-        
+
         // write to interface.hpp
 
         // signal class
@@ -292,33 +283,37 @@ VerilatorAPI::genInterfaceFile(){
         output_file << "public:" << endl;
         output_file << "\tInterface( Vtestf___024root* rootp ){" << endl;
         output_file << "\t\t// Primary Input" << endl;
-        for (string pi : input_vec){
-            size_t clk_found = pi.find("clk");
+        for (string pi : input_vec) {
+            size_t           clk_found = pi.find("clk");
             array<string, 2> rst_name{"reset", "rst"};
-            auto rst_found = find_if(begin(rst_name), end(rst_name), [&](const string& s){return pi.find(s) != string::npos; });
+            auto             rst_found = find_if(begin(rst_name), end(rst_name), [&](const string& s) { return pi.find(s) != string::npos; });
             if (clk_found != string::npos)
-                output_file << "\t\tclk.push_back(new SignalC" << "(\"" << pi << "\", &rootp->" << pi << ", 1));" << endl;
+                output_file << "\t\tclk.push_back(new SignalC"
+                            << "(\"" << pi << "\", &rootp->" << pi << ", 1));" << endl;
             else if (rst_found != end(rst_name))
-                output_file << "\t\trst.push_back(new SignalC" << "(\"" << pi << "\", &rootp->" << pi << ", 1));" << endl;
+                output_file << "\t\trst.push_back(new SignalC"
+                            << "(\"" << pi << "\", &rootp->" << pi << ", 1));" << endl;
             else
-                output_file << "\t\tpi.push_back(new Signal" << signalWidth2Type(name2bit_map[pi]) << "(\"" << pi << "\", &rootp->" << pi << ", " << name2bit_map[pi] << "));" << endl;
+                output_file << "\t\tpi.push_back(new Signal" << signalWidth2Type(name2bit_map[pi]) << "(\"" << pi << "\", &rootp->" << pi << ", "
+                            << name2bit_map[pi] << "));" << endl;
         }
         output_file << "\t\t// Primary Output" << endl;
-        for (string po : output_vec){
-            output_file << "\t\tpo.push_back(new Signal" << signalWidth2Type(name2bit_map[po]) << "(\"" << po << "\", &rootp->" << po << ", " << name2bit_map[po] << "));" << endl;
+        for (string po : output_vec) {
+            output_file << "\t\tpo.push_back(new Signal" << signalWidth2Type(name2bit_map[po]) << "(\"" << po << "\", &rootp->" << po << ", "
+                        << name2bit_map[po] << "));" << endl;
         }
-
 
         // Search registers
         output_file << "\t\t// Registers" << endl;
         smatch m;
-        regex reg_line ("gvEx_r");
-        for (string reg : output_vec){
+        regex  reg_line("gvEx_r");
+        for (string reg : output_vec) {
             regex_search(reg, m, reg_line);
             if (!m.empty())
-                output_file << "\t\treg.push_back(new Signal" << signalWidth2Type(name2bit_map[reg]) << "(\"" << reg << "\", &rootp->" << reg << ", " << name2bit_map[reg] << "));" << endl;
+                output_file << "\t\treg.push_back(new Signal" << signalWidth2Type(name2bit_map[reg]) << "(\"" << reg << "\", &rootp->" << reg << ", "
+                            << name2bit_map[reg] << "));" << endl;
         }
-        //output_file << "//Register" << endl;
+        // output_file << "//Register" << endl;
         output_file << "\t}" << endl;
         output_file << "\tstd::vector<Signal*> clk;" << endl;
         output_file << "\tstd::vector<Signal*> rst;" << endl;
@@ -330,26 +325,24 @@ VerilatorAPI::genInterfaceFile(){
     }
     input_file.close();
     output_file.close();
-
 }
 
 // Collecting assertion for all file
 vector<string>
-VerilatorAPI::collectAssertion(string filename){
-    ifstream input_file(filename);
-    ofstream output_file("obj_dir/design_under_test.v");
+VerilatorAPI::collectAssertion(string filename) {
+    ifstream       input_file(filename);
+    ofstream       output_file("obj_dir/design_under_test.v");
     vector<string> assertion_vec;
-    string output_path;
-    string line;
-    string assertion_info;
-    string result;
-    regex assertion_line ("assert\s*\\((.*)\\)\s*;");
-    regex assertion_word ("assert");
-    regex semicolon_regex(";");
-    smatch m;
-    int line_num, assertion_cnt=0;
-    string dpi_template = \
-    "// DPI Header\
+    string         output_path;
+    string         line;
+    string         assertion_info;
+    string         result;
+    regex          assertion_line("assert\s*\\((.*)\\)\s*;");
+    regex          assertion_word("assert");
+    regex          semicolon_regex(";");
+    smatch         m;
+    int            line_num, assertion_cnt = 0;
+    string         dpi_template = "// DPI Header\
     \rimport \"DPI-C\" function void set_assert_flag (input int index);\
     \rimport \"DPI-C\" function void set_constraint_flag (input int index);\
     \rimport \"DPI-C\" function void set_stable_flag (input int index);\n";
@@ -358,15 +351,15 @@ VerilatorAPI::collectAssertion(string filename){
     line_num = 1;
 
     output_file << dpi_template << endl;
-    while(getline(input_file, line)){
+    while (getline(input_file, line)) {
         regex_search(line, m, assertion_line);
-        if (!m.empty()){   
-            result = regex_replace(line, assertion_word, "Assertion_" + to_string(assertion_cnt));
-            result = regex_replace(result, semicolon_regex, " else set_assert_flag\(" + to_string(assertion_cnt) + "\)");
+        if (!m.empty()) {
+            result         = regex_replace(line, assertion_word, "Assertion_" + to_string(assertion_cnt));
+            result         = regex_replace(result, semicolon_regex, " else set_assert_flag\(" + to_string(assertion_cnt) + "\)");
             assertion_info = filename + ", " + to_string(assertion_cnt) + ", " + to_string(line_num) + ", " + m[1].str();
             assertion_vec.push_back(assertion_info);
-            //cout << assertion_info << endl;
-            //cout << result << endl;
+            // cout << assertion_info << endl;
+            // cout << result << endl;
             assertion_cnt += 1;
         }
         output_file << line << endl;
@@ -378,7 +371,7 @@ VerilatorAPI::collectAssertion(string filename){
 }
 
 void
-VerilatorAPI::genAssertionFile(vector<string> assertion_vec){
+VerilatorAPI::genAssertionFile(vector<string> assertion_vec) {
     gvMsg(GV_MSG_IFO) << "[INFO] Generating assertion.hpp ..." << endl;
     ofstream output_file("/obj_dir/assertion.hpp");
     output_file << "#ifndef assertion_hpp" << endl;
@@ -387,7 +380,7 @@ VerilatorAPI::genAssertionFile(vector<string> assertion_vec){
     output_file << "class Assertion{" << endl;
     output_file << "public:" << endl;
     output_file << "\tAssertion(){" << endl;
-    for(string &assertion : assertion_vec){
+    for (string& assertion : assertion_vec) {
         output_file << "\t\tassertion.emplace(assertion.end(), " << assertion << ")" << endl;
     }
     output_file << "\t}" << endl;
@@ -396,14 +389,15 @@ VerilatorAPI::genAssertionFile(vector<string> assertion_vec){
     output_file << "#endif /* assertion_hpp */" << endl;
 }
 
-VerilatorAPI::VerilatorAPI(string testbench, int shm_size){
+VerilatorAPI::VerilatorAPI(string testbench, int shm_size) {
     key_t key;
 
     // IPC - Shared Memory
-    key= 7899;
-    VerilatorAPI::_shmid= shmget(key, shm_size, IPC_CREAT|0666);
-    if(VerilatorAPI::_shmid == -1) {
-        cout << strerror(errno) << endl;;
+    key                  = 7899;
+    VerilatorAPI::_shmid = shmget(key, shm_size, IPC_CREAT | 0666);
+    if (VerilatorAPI::_shmid == -1) {
+        cout << strerror(errno) << endl;
+        ;
         perror("[ERR] GV - Error in Shared Memory get statement");
         exit(1);
     }
@@ -411,19 +405,18 @@ VerilatorAPI::VerilatorAPI(string testbench, int shm_size){
     system("mkdir obj_dir");
     VerilatorAPI::getDesignInfo();
     VerilatorAPI::genInterfaceFile();
-    VerilatorAPI::verilogModeling(testbench); 
+    VerilatorAPI::verilogModeling(testbench);
     cout << "[INFO] GV - Build Verilator Env." << endl;
 }
 
 VerilatorAPI::~VerilatorAPI() {
-    cout << "[INFO] Delete shared memory => (id=" << VerilatorAPI::get_shmid() << ")"<< endl;
-    //string system_cmd;
-    //system_cmd = "ipcrm -m " + to_string(VerilatorAPI::get_shmid());
-    //cout << "[DBG] GV - Deconstruct " << system_cmd << endl;
-    //system(system_cmd.c_str());
+    cout << "[INFO] Delete shared memory => (id=" << VerilatorAPI::get_shmid() << ")" << endl;
+    // string system_cmd;
+    // system_cmd = "ipcrm -m " + to_string(VerilatorAPI::get_shmid());
+    // cout << "[DBG] GV - Deconstruct " << system_cmd << endl;
+    // system(system_cmd.c_str());
     if (shmctl(VerilatorAPI::get_shmid(), IPC_RMID, 0) == -1) {
         fprintf(stderr, "[ERR] GV - shmctl(IPC_RMID) failed\n");
         exit(EXIT_FAILURE);
     }
 }
-
--- src/sim/verilator_api/verilator_api.cpp
+++ src/sim/verilator_api/verilator_api.cpp
@@ -1,81 +1,67 @@
+#include "argparse.hpp"
 #include "simulator.hpp"
 #include "string"
-#include <sys/types.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <errno.h>
 #include <iostream>
-#include "argparse.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[]) {
+int
+main(int argc, char* argv[]) {
 
-    Simulator *simulator = new Simulator();
-    unsigned RegNum = simulator->getRegNum();
-    unsigned PiNum = simulator->getPiNum();
-    unsigned PoNum = simulator->getPoNum();
-	int shmid;
-	key_t key;
-	char *shm, *s;
+    Simulator* simulator = new Simulator();
+    unsigned   RegNum    = simulator->getRegNum();
+    unsigned   PiNum     = simulator->getPiNum();
+    unsigned   PoNum     = simulator->getPoNum();
+    int        shmid;
+    key_t      key;
+    char *     shm, *s;
 
     // IPC - Shared Memory
-	key= 7899;
-    //size_t shm_size = PiNum + RegNum + PoNum;
+    key = 7899;
+    // size_t shm_size = PiNum + RegNum + PoNum;
     size_t shm_size = 1000000;
     cout << "[INFO] Size of Shared Memory: " << shm_size << endl;
-	shmid= shmget(key, shm_size, 0666);
-	if(shmid == -1) {
-		cout << strerror(errno) << endl;;
-		perror("Error in Shared Memory get statement");
-		exit(1);
-	}
+    shmid = shmget(key, shm_size, 0666);
+    if (shmid == -1) {
+        cout << strerror(errno) << endl;
+        ;
+        perror("Error in Shared Memory get statement");
+        exit(1);
+    }
 
-	shm= (char*) shmat(shmid, NULL, 0);
-	if(shm == (char*) -1) {
-		cout << strerror(errno) << endl;
-		perror("Error in Shared Memory attachment");
-		exit(1);
-	}
+    shm = (char*)shmat(shmid, NULL, 0);
+    if (shm == (char*)-1) {
+        cout << strerror(errno) << endl;
+        perror("Error in Shared Memory attachment");
+        exit(1);
+    }
 
     // Argument Parser
     argparse::ArgumentParser program("verilator_api");
-    
+
     // reset
-    program.add_argument("-R", "--reset")
-        .default_value(false)
-        .help("whether simulator reset design or not")
-        .implicit_value(true);
-     
+    program.add_argument("-R", "--reset").default_value(false).help("whether simulator reset design or not").implicit_value(true);
+
     // sequence
-    program.add_argument("-S", "--sequence")
-        .default_value(false)
-        .help("return pattern sequence of design")
-        .implicit_value(true);
+    program.add_argument("-S", "--sequence").default_value(false).help("return pattern sequence of design").implicit_value(true);
 
     // update
-    program.add_argument("-U", "--update")
-        .default_value(false)
-        .help("update current state to verilator")
-        .implicit_value(true);
+    program.add_argument("-U", "--update").default_value(false).help("update current state to verilator").implicit_value(true);
 
     // eval
-    program.add_argument("-E", "--eval")
-        .default_value(0)
-        .help("eval n cycle")
-        .scan<'i', int>();
+    program.add_argument("-E", "--eval").default_value(0).help("eval n cycle").scan<'i', int>();
 
     // printState
-    program.add_argument("-P", "--print")
-        .default_value(false)
-        .help("print simulator info")
-        .implicit_value(true);
+    program.add_argument("-P", "--print").default_value(false).help("print simulator info").implicit_value(true);
 
     try {
         program.parse_args(argc, argv);
-    }
-    catch (const runtime_error& err) {
+    } catch (const runtime_error& err) {
         cerr << err.what() << endl;
         cerr << program;
         exit(1);
@@ -103,26 +89,25 @@ int main(int argc, char *argv[]) {
         string pattern_str(shm);
         string token;
         size_t pos = pattern_str.find(" ");
-        token = pattern_str.substr(0, pos);
+        token      = pattern_str.substr(0, pos);
         cout << "[INFO] Verilator - Shared memory (id=" << shmid << ") => Read data (size=" << token.size() << ")" << endl;
         cout << token << endl;
         vector<unsigned> pattern = simulator->str2pattern(token);
         simulator->setAllPattern(pattern);
     }
 
-    
     auto eval_cycle = program.get<int>("eval");
     if (eval_cycle != 0) {
         string pattern_str(shm);
         string token;
         size_t pos = pattern_str.find(" ");
-        token = pattern_str.substr(0, pos);
+        token      = pattern_str.substr(0, pos);
         cout << "[INFO] Verilator - Shared memory (id=" << shmid << ") => Read data (size=" << token.size() << ")" << endl;
         cout << token << endl;
         vector<unsigned> pattern = simulator->str2pattern(token);
         simulator->setAllPattern(pattern);
         cout << "[INFO] Verilator - eval " << eval_cycle << " cycle" << endl;
-        for (unsigned i=0; i<eval_cycle; i++){
+        for (unsigned i = 0; i < eval_cycle; i++) {
             simulator->evalOneClock();
         }
         vector<unsigned> AllPattern;
@@ -137,14 +122,14 @@ int main(int argc, char *argv[]) {
         string pattern_str(shm);
         string token;
         size_t pos = pattern_str.find(" ");
-        token = pattern_str.substr(0, pos);
+        token      = pattern_str.substr(0, pos);
         cout << "[INFO] Verilator - Shared memory (id=" << shmid << ") => Read data (size=" << token.size() << ")" << endl;
         cout << token << endl;
         vector<unsigned> pattern = simulator->str2pattern(token);
         simulator->setAllPattern(pattern);
         simulator->printPiPattern();
         simulator->printRegPattern();
-        //simulator->printPoPattern();
+        // simulator->printPoPattern();
     }
 
     return 0;
