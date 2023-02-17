06749936bbb03f02044a0ac572b629aee2413a1a
hchchiu <robert890619@gmail.com>
2023-02-17 21:41:09 +0800

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Install prerequisites for building yosys
-sudo apt-get install build-essential clang bison flex \
-	libreadline-dev gawk tcl-dev libffi-dev git \
-	graphviz xdot pkg-config python3 libboost-system-dev \
-	libboost-python-dev libboost-filesystem-dev zlib1g-dev
+sudo apt-get install gperf build-essential bison flex libreadline-dev gawk tcl-dev libffi-dev graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev
 
 # Make install yosys
 if [ -f "/usr/local/lib/yosys/libyosys.so" ]; then
